ba8bf3b321e5bb5ff8e2ca1c7308afc89c6922cb
pedro-emidio-dr <pedro.emidio@datarain.com.br>
2023-10-25 15:04:14 -0300

    updata main

---
+++ terraform/identity-center-account.tf
@@ -0,0 +1,27 @@
+module "identity-center" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "pedro.emidio+bmg-lz-identity-center@datarain.com.br"
+    AccountName               = "Identity-center"
+    ManagedOrganizationalUnit = "Security"
+    SSOUserEmail              = "pedro.emidio+bmg-lz-identity-center@datarain.com.br"
+    SSOUserFirstName          = "Pedro"
+    SSOUserLastName           = "Emidio"
+  }
+
+  account_tags = {
+    "CreatedBy" = "Terraform"
+  }
+
+  custom_fields = {
+    group = "controller"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "identity center"
+    change_reason       = "identity center management"
+  }
+
+  account_customizations_name = "controller"
+}
