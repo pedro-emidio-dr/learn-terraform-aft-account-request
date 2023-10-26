dcb369e00c15e14c017afbce65697fc3863adb2e
pedro-emidio-dr <pedro.emidio@datarain.com.br>
2023-10-26 16:41:54 -0300

    update reason

--- terraform/network-account.tf
+++ terraform/network-account.tf
@@ -2,10 +2,10 @@ module "network" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "pedro.emidio+bmg-lz-network-configs@datarain.com.br"
+    AccountEmail              = "pedro.emidio+bmg-lz-network@datarain.com.br"
     AccountName               = "Network"
     ManagedOrganizationalUnit = "r-rhrc"
-    SSOUserEmail              = "pedro.emidio+bmg-lz-network-configs@datarain.com.br"
+    SSOUserEmail              = "pedro.emidio+bmg-lz-network@datarain.com.br"
     SSOUserFirstName          = "Pedro"
     SSOUserLastName           = "Emidio"
   }
