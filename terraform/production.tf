151893639af8ec0638ba8bb5d70e0ff994947f5a
pedro-emidio-dr <pedro.emidio@datarain.com.br>
2024-01-11 11:42:45 -0300

    delete production account

--- terraform/production.tf
+++ terraform/production.tf
@@ -1,28 +1,28 @@
-module "production" {
-  source = "./modules/aft-account-request"
+# module "production" {
+#   source = "./modules/aft-account-request"
 
-  control_tower_parameters = {
-    AccountEmail              = "pedro.emidio+bmg-lz-prd@datarain.com.br"
-    AccountName               = "Production"
-    ManagedOrganizationalUnit = "Workloads"
-    SSOUserEmail              = "pedro.emidio+bmg-lz-prd@datarain.com.br"
-    SSOUserFirstName          = "Pedro"
-    SSOUserLastName           = "Emidio"
-  }
+#   control_tower_parameters = {
+#     AccountEmail              = "pedro.emidio+bmg-lz-prd@datarain.com.br"
+#     AccountName               = "Production"
+#     ManagedOrganizationalUnit = "Workloads"
+#     SSOUserEmail              = "pedro.emidio+bmg-lz-prd@datarain.com.br"
+#     SSOUserFirstName          = "Pedro"
+#     SSOUserLastName           = "Emidio"
+#   }
 
-  account_tags = {
-    "CreatedBy" = "Terraform"
-  }
+#   account_tags = {
+#     "CreatedBy" = "Terraform"
+#   }
 
-  custom_fields = {
-    group = "production"
-    teste = "teste"
-  }
+#   custom_fields = {
+#     group = "production"
+#     teste = "teste"
+#   }
 
-  change_management_parameters = {
-    change_requested_by = "production"
-    change_reason       = "production account"
-  }
+#   change_management_parameters = {
+#     change_requested_by = "production"
+#     change_reason       = "production account"
+#   }
 
-  account_customizations_name = "workloads"
-}
+#   account_customizations_name = "workloads"
+# }
